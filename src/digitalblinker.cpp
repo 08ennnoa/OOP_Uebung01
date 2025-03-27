5f3335ef4dd30b391c04cbff6a4634a36fc2c464
Noah Ennemoser <08ennnoa@rgtfo-me.it>
2025-03-27 11:53:53 +0100

    Cpp u. h des digitalen Blinkers

--- src/digitalblinker.cpp
+++ src/digitalblinker.cpp
@@ -1,61 +1,27 @@
-#ifndef DIGITALBLINKER_H
-#define DIGITALBLINKER_H
-
 #include <Arduino.h>
 #include "digitalblinker.h"
-
-void blinken::init(unsigned long _blinktime,
-    uint8_t _led1,
-    bool _inv1,
-    uint8_t _led2,
-    bool _inv2,
-    uint8_t _led3,
-    bool _inv3,
-    bool _enable)
+void blinker::init(unsigned long _blinktime, bool _enable, uint8_t _pin1, uint8_t _pin2)
 {
-blinktime = _blinktime;
-led1 = _led1;
-inv1 = _inv1;
-led2 = _led2;
-inv2 = _inv2;
-led3 = _led3;
-inv3 = _inv3;
-enable = _enable;
-
-digitalWrite(led1, inv1 ? HIGH : LOW);
-digitalWrite(led2, inv2 ? HIGH : LOW);
-digitalWrite(led3, inv3 ? HIGH : LOW);
+    blinktime = _blinktime;
+    enable = _enable;
+    pin1 = _pin1;
+    pin2 = _pin2;
 }
 
-void blinken::poll()
+void blinker::poll()
 {
-if (!enable)
-{
-return;
+    if (!enable)
+    {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, HIGH);
+        return;
+    }
+
+    if (millis() - last > blinktime)
+    {
+        state = !state;
+        digitalWrite(pin1, state);
+        digitalWrite(pin2, !state);
+        last = millis();
+    }
 }
-
-if (millis() - last > blinktime)
-{
-state = !state;
-digitalWrite(led1, state ? !inv1 : inv1);
-digitalWrite(led2, state ? !inv2 : inv2);
-digitalWrite(led3, state ? !inv3 : inv3);
-
-last = millis();
-}
-}
-
-void blinken::off()
-{
-digitalWrite(led1, inv1 ? HIGH : LOW);
-digitalWrite(led2, inv2 ? HIGH : LOW);
-digitalWrite(led3, inv3 ? HIGH : LOW);
-}
-
-
-
-
-
-
-
-#endif
